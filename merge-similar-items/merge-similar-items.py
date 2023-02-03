597e90edfdfe5790e0fd11212b8d1aaab4c71e43
OanhBui <oanhbp3105@gmail.com>
2023-02-03 12:29:17 -0800

    update Solution using Counter

--- merge-similar-items/merge-similar-items.py
+++ merge-similar-items/merge-similar-items.py
@@ -31,3 +31,10 @@ class Solution:
         return sorted(list(weights.items()))
 
 
+#Solution3 using Counter
+from collections import Counter
+
+class Solution:
+    def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
+        weights = Counter(dict(items1)) + Counter(dict(items2))
+        return sorted(list(weights.items()))
