fe9629d3a0353041b7d999635b525db234d4ab92
OanhBui <oanhbp3105@gmail.com>
2023-02-03 12:34:16 -0800

    update JS solution using Map()

--- merge-similar-items/merge-similar-items.js
+++ merge-similar-items/merge-similar-items.js
@@ -14,3 +14,18 @@ var mergeSimilarItems = function(items1, items2) {
     return Object.entries(weights).sort((a, b) => a[0] - b[0])
     
 };
+
+//Solution2 using Map()
+var mergeSimilarItems = function(items1, items2) {
+    const weights = new Map();
+    for (const [value, weight] of [...items1, ...items2]) {
+        if (weights.get(value) === undefined) {
+            weights.set(value, weight)
+        } else {
+            weights.set(value, weights.get(value) + weight)
+        }
+    }
+
+    return Array.from(weights).sort((a, b) => a[0] - b[0])
+    
+};
