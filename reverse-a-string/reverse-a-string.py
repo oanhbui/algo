ee6af911ddf54fafcc6bb52034e7681b2dfc94c3
OanhBui <oanhbp3105@gmail.com>
2023-02-09 00:11:36 -0800

    fix bug

--- reverse-a-string/reverse-a-string.py
+++ reverse-a-string/reverse-a-string.py
@@ -1,8 +1,16 @@
-def reverse_a_string(str):
-    char_array = list(str)
-    for i in range(floor(len(char_array)/2)):
+def reverse_a_string(s):
+    
+    char_array = list(s)
+    for i in range((len(char_array) // 2)):
         left_char = char_array[i]
-        right_char = char_array[len(char_array) - i]
+        right_char = char_array[len(char_array) - 1 - i]
         char_array[i] = right_char
-        char_array[len(char_array) - i] = left_char
+        char_array[len(char_array) - 1 - i] = left_char
     return ''.join(char_array)
+
+
+assert reverse_a_string('') == ''
+assert reverse_a_string('a') == 'a'
+assert reverse_a_string('abc') == 'cba'
+assert reverse_a_string('abcd') == 'dcba'
+assert reverse_a_string('abc defg') == 'gfed cba'
