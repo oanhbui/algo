8baaa07b3162a1630dc285d237ee252ba3cc30ac
OanhBui <oanhbp3105@gmail.com>
2023-02-22 23:44:51 -0800

    merge-sorted-arrays python #2

--- merge-sorted-arrays/merge-sorted-arrays.py
+++ merge-sorted-arrays/merge-sorted-arrays.py
@@ -1,8 +1,35 @@
 #1
 
-def merge_sorted_array(array1, array2):
+def merge_sorted_array1(array1, array2):
     array1.extend(array2)
     array1.sort()
     return array1
 
-print(merge_sorted_array([1, 2, 3, 4, 5], [3, 6, 7]))
+#print(merge_sorted_array1([1, 2, 3, 4, 5], [3, 6, 7]))
+
+#2
+
+def merge_sorted_array2(array1, array2):
+    len1 = len(array1)
+    len2 = len(array2)
+    pointer1 = 0
+    pointer2 = 0
+    final = []
+    while pointer1 < len1 and pointer2 < len2:
+        if array1[pointer1] <= array2[pointer2]:
+            final.append(array1[pointer1])
+            pointer1 += 1
+        else:
+            final.append(array2[pointer2])
+            pointer2 += 1
+    if pointer1 < len1:
+        while pointer1 < len1:
+            final.append(array1[pointer1])
+            pointer1 += 1
+    else:
+        while pointer2 < len2:
+            final.append(array2[pointer2])
+            pointer2 += 1
+    return final
+        
+print(merge_sorted_array2([-4, -2], [3, 6, 7, 8, 9]))
