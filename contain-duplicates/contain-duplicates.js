b0380a36d280775ae66a9c0fa98127ed6a2be623
OanhBui <oanhbp3105@gmail.com>
2023-03-01 10:47:49 -0800

    contain-duplicates using hash set

--- contain-duplicates/contain-duplicates.js
+++ contain-duplicates/contain-duplicates.js
@@ -1,5 +1,7 @@
 /*https://leetcode.com/problems/contains-duplicate/*/
 
+//1
+
 var containsDuplicate = function(nums) {
     for (let i = 0; i < nums.length; i++) {
         const model = nums[i];
@@ -9,6 +11,25 @@ var containsDuplicate = function(nums) {
                 break
             }
         }
+    }
+    return false
+};
+
+
+//2
+var containsDuplicate = function(nums) {
+    const seen = new Set(nums);
+    return seen.size < nums.length
+};
+
+//3
+var containsDuplicate = function(nums) {
+    const seen = new Set();
+    for (const num of nums) {
+        if (seen.has(num)) {
+            return true
+        }
+        seen.add(num)
     }
     return false
 };
