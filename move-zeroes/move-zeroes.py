da47ba90005c4cd1fa0d302ce853e2b076f494b6
OanhBui <oanhbp3105@gmail.com>
2023-02-28 21:44:07 -0800

    upload move-zeroes.py

---
+++ move-zeroes/move-zeroes.py
@@ -0,0 +1,11 @@
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        w = 0
+        n = len(nums)
+        for r in range(0, n):
+            if nums[r] != 0:
+                nums[r], nums[w] = nums[w], nums[r]
+                w += 1
+        return nums
