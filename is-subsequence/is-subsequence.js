9ae97625c3cfbb5a62ae01c01769698b1cf22675
OanhBui <oanhbp3105@gmail.com>
2023-03-08 11:21:14 -0800

    is-subsequence.js #2 with separated function to reuse

--- is-subsequence/is-subsequence.js
+++ is-subsequence/is-subsequence.js
@@ -21,3 +21,30 @@ var isSubsequence = function(s, t) {
     };
     return true;
 };
+
+//*2 (same as *1 but separated function to reuse)
+
+function findFrom(haystack, needle, index) {
+    let i = index;
+    while (i < haystack.length) {
+        if (haystack[i] === needle) {
+            return i;
+        }
+        i += 1;
+    };
+    return -1
+}
+
+var isSubsequence = function(s, t) {
+    let i = 0;
+    let j = 0;
+    while (i < s.length) {
+        j = findFrom(t, s[i], j);
+        if (j < 0) {
+            return false;
+        }
+        i += 1;
+        j += 1;
+    };
+    return true;
+};
