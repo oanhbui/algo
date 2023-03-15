3b1d86946903c7a48a2f101b73973d8af31d473b
OanhBui <oanhbp3105@gmail.com>
2023-03-14 17:07:38 -0700

    binary-search.js

---
+++ binary-search/binary-search.js
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/binary-search/description/
+
+var search = function(nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
+    while (low <= high) {
+        let mid = Math.floor((high + low) / 2);
+        if (nums[mid] === target) {
+            return mid
+        } else if (nums[mid] > target) {
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+    }
+    return -1
+};
