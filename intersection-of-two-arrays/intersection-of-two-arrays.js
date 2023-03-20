22f7597f9bf5cc0acd286c2d2beca2aadde2a1f5
OanhBui <oanhbp3105@gmail.com>
2023-03-20 11:28:42 -0700

    intersection-of-two-arrays.js #1

---
+++ intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/intersection-of-two-arrays/
+
+
+//**1
+var intersection = function(nums1, nums2) {
+    const seen = new Set(nums1);
+    const uniqueNums2 = new Set(nums2);
+    let result = [];
+    for (const num of uniqueNums2) {
+        if (seen.has(num)) {
+            result.push(num)
+        }
+    };
+    return result
+};
+
+
