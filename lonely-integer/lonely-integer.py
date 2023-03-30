20e8eea736a46f6bf09ffc306690575666ae2940
OanhBui <oanhbp3105@gmail.com>
2023-03-29 22:53:17 -0700

    lonely-integer.py #2 using set()

--- lonely-integer/lonely-integer.py
+++ lonely-integer/lonely-integer.py
@@ -1,7 +1,7 @@
 #https://www.hackerrank.com/challenges/three-month-preparation-kit-lonely-integer/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
 
 
-#1
+#1 using map function
 def lonelyinteger(a):
     # Write your code here
     seen = {}
@@ -13,3 +13,14 @@ def lonelyinteger(a):
     for (key, value) in seen.items():
         if value == 1:
             return key
+        
+#2 using set()
+def lonelyinteger(a):
+    # Write your code here
+    seen = set()
+    for num in a:
+        if num in seen:
+            seen.discard(num)
+        else:
+            seen.add(num)
+    return seen.pop()
