15dd7c71381a3b524ec2c89b1b6ddd6e63682a03
OanhBui <oanhbp3105@gmail.com>
2023-04-24 16:53:50 -0700

    hackerRank permuting-two-arrays.js

---
+++ permuting-two-arrays/permuting-two-arrays.js
@@ -0,0 +1,14 @@
+//HackerRank - Permuting Two Arrays
+
+function twoArrays(k, A, B) {
+    // Write your code here
+    A.sort((a, b) => a - b);
+    B.sort((a, b) => b - a);
+    for (let i = 0; i < A.length; i++) {
+        if (A[i] + B[i] < k) {
+            return 'NO'
+        }
+    };
+    return 'YES'
+
+}
