d81d32732a22311fdd95dbd39ee5039df416150b
OanhBui <oanhbp3105@gmail.com>
2023-04-25 11:23:33 -0700

    subarray-division-2.js method 2

--- subarray-division-2/subarray-division-2.js
+++ subarray-division-2/subarray-division-2.js
@@ -19,3 +19,25 @@ function birthday(s, d, m) {
     };
     return methodCount
 }
+
+//2
+function birthday(s, d, m) {
+    // Write your code here
+    if (s.length < m) {
+        return 0
+    };
+    let trackSum = 0;
+    let methodCount = 0;
+    let start = 0;
+    for (let i = 0 ; i < s.length; i++) {
+        trackSum += s[i];
+        if (i >= m - 1) {
+            if (trackSum === d) {
+                methodCount += 1
+            };
+            trackSum = trackSum - s[start];
+            start += 1
+        }
+    };
+    return methodCount
+}
