e995a2b0fbda7984418cf30043537e708b8abc1e
OanhBui <oanhbp3105@gmail.com>
2023-04-27 10:43:40 -0700

    hackerRank double-size.js using Set()

--- double-size/double-size.js
+++ double-size/double-size.js
@@ -8,3 +8,14 @@ function doubleSize(arr, num) {
     return num
 };
 console.log(doubleSize([1, 2, 4, 11, 12, 8], 2))
+
+
+//2
+
+function doubleSize(arr, num) {
+    const seen = new Set(arr);
+    while (seen.has(num)) {
+        num = num * 2
+    }
+    return num
+};
