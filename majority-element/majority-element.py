33c26f900a24dcea2189983af86d74a3de25b181
OanhBui <oanhbp3105@gmail.com>
2023-06-14 22:36:04 -0700

    leetcode majority-element.py method 2

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,5 +1,7 @@
 #leetcode - majority element (easy)
 
+#1
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         frequency_counter = {}
@@ -8,3 +10,20 @@ class Solution:
         for key, value in frequency_counter.items():
             if value > len(nums) / 2:
                 return key
+            
+
+#2
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        sorted_nums = sorted(nums)
+        half = len(nums) / 2
+        count = 1
+        for i in range(1, len(nums)):
+            if sorted_nums[i] == sorted_nums[i - 1]:
+                count += 1
+            else:
+                count = 1
+            if count > half:
+                return sorted_nums[i]
+        return nums[0]
