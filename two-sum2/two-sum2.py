2d8048a2e18270606ea40d3c8996ad465989aa8b
OanhBui <oanhbp3105@gmail.com>
2023-09-08 12:12:27 -0700

    twosum2.py using binary search

--- two-sum2/two-sum2.py
+++ two-sum2/two-sum2.py
@@ -15,3 +15,26 @@ class Solution:
                 hi -= 1
             else:
                 lo += 1 
+
+
+#method 2 - using binary search
+
+class Solution:
+    def find_addup(self, numbers, addup, start):
+        lo = start
+        hi = len(numbers) - 1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if numbers[mid] == addup:
+                return mid
+            elif numbers[mid] > addup:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return -1
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for (index, num) in enumerate(numbers):
+            addup = target - num
+            to_find = self.find_addup(numbers, addup, index + 1)
+            if to_find != -1:
+                return [index + 1, to_find + 1]
