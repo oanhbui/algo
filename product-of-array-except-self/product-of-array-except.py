59a12e925c47c4d21318b009444a416d0864edbd
OanhBui <oanhbp3105@gmail.com>
2023-10-02 12:30:55 -0700

    product of array except self #2

--- product-of-array-except-self/product-of-array-except.py
+++ product-of-array-except-self/product-of-array-except.py
@@ -1,5 +1,6 @@
 #leetcode - product of Array except self - medium
 
+#1
 def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
         from_left = 1
@@ -19,4 +20,27 @@ def productExceptSelf(self, nums: List[int]) -> List[int]:
                 result[i] = left_product[n - 2]
             else:
                 result[i] = left_product[i - 1] * right_product[i + 1]
+        return result
+
+#2
+def productExceptSelf(self, nums: List[int]) -> List[int]:
+        zero_count = 0
+        nonzero_product = 1
+        for num in nums:
+            if num == 0:
+                zero_count += 1
+            else:
+                nonzero_product *= num
+        result = [0] * len(nums)
+        if zero_count >= 2:
+            return result
+        if zero_count == 1:
+            for i in range(len(nums)):
+                if nums[i] != 0:
+                    result[i] = 0
+                else:
+                    result[i] = nonzero_product
+            return result
+        for i in range(len(nums)):
+            result[i] = nonzero_product // nums[i]
         return result
