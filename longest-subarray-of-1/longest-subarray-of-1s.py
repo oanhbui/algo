7618b6847260bcb6d9b01b1b58302aba9132799e
OanhBui <oanhbp3105@gmail.com>
2023-11-08 23:21:21 -0800

    longest subarray of 1s - leetcode - medium - solution 2

--- longest-subarray-of-1/longest-subarray-of-1s.py
+++ longest-subarray-of-1/longest-subarray-of-1s.py
@@ -17,3 +17,22 @@ class Solution:
             length = right - left
             max_length = max(max_length, length)
         return max_length - 1
+
+
+#2
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        zero_count = 0
+        max_length = 0
+        left = -1
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zero_count += 1
+            while left < right and zero_count > 1:
+                left += 1
+                if nums[left] == 0:
+                    zero_count -= 1
+                
+            length = right - left
+            max_length = max(max_length, length)
+        return max_length - 1
