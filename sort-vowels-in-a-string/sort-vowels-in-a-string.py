75293826c5916d8c5bafd8a85192a3aba9b2760f
OanhBui <oanhbp3105@gmail.com>
2023-11-13 11:16:19 -0800

    leetcode - medium - sort vowels in a string - method 2 improved runtime to On

--- sort-vowels-in-a-string/sort-vowels-in-a-string.py
+++ sort-vowels-in-a-string/sort-vowels-in-a-string.py
@@ -17,3 +17,25 @@ def sortVowels(self, s: str) -> str:
                 s_list[i] = s_vowels[check]
                 check += 1
         return "".join(s_list)
+
+#2
+
+def sortVowels(self, s: str) -> str:
+        
+        vowels = "AEIOUaeiou"
+        vowels_by_index = {}
+        for index, char in enumerate(s):
+            if char in vowels:
+                vowels_by_index[char] = vowels_by_index.get(char, 0) + 1
+        vowels_sorted = []
+        for char in vowels:
+            if char in vowels_by_index:
+                vowels_sorted.extend([char] * vowels_by_index[char])
+        s_list = list(s)
+        check = 0
+        print(vowels_sorted)
+        for i in range(len(s_list)):
+            if s_list[i] in vowels_by_index:
+                s_list[i] = vowels_sorted[check]
+                check += 1
+        return "".join(s_list)
